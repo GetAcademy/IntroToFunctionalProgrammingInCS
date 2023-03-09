c76bbec98b6e21d7cb20da44d7e6e08e726fc947
GET Academy <terje@getacademy.no>
2023-03-09 11:56:20 +0100

    Update Program.cs

--- IntroToFunctionalProgrammingInCS/Program.cs
+++ IntroToFunctionalProgrammingInCS/Program.cs
@@ -1,5 +1,19 @@
 ﻿using IntroToFunctionalProgrammingInCS;
 
+/*
+ Hva er funksjonell programmering?
+ - rene funksjoner - ingen sideeffekter
+ - immutable state (ren og uren verden)
+ - høyere ordens-funksjoner
+
+ - point free functions
+ - currying
+ - partial application
+ - litt om typer (eksempel på Option under) + si litt om algebraisk typesystem
+
+ - class vs struct vs record
+
+ */
 //Demo1ImpureFunctions.Run();
 //Demo2PureFunctions.Run();
 //Demo3HonestAndPureFunctions.Run();
@@ -7,4 +21,3 @@
 //Demo5CurryingAndPartialApplication.Run();
 //Demo6PointfreePipeAndComposition.Run();
 //Demo7OptionType.Run();
-//class vs struct vs record
